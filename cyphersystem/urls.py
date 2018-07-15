2a64837fa443de39fd0dac7ad966da2b4526629e
michaelbirdtx <michaelbirdtx@icloud.com>
2018-07-15 00:50:33 -0500

    Added root site link to cyphercore w/logo. More UI tweaks.

--- cyphersystem/urls.py
+++ cyphersystem/urls.py
@@ -17,8 +17,10 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('cyphercore/', include('cyphercore.urls')),
     path('admin/', admin.site.urls),
 ]
