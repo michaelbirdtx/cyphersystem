050d1f343e791c502fce8d74a8e75721ba4a17dd
Michael Bird <michael@Michaels-iMac.local>
2018-07-30 16:10:34 -0500

    Removed root-level static directory.

--- cyphersystem/settings.py
+++ cyphersystem/settings.py
@@ -128,9 +128,6 @@ INTERNAL_IPS = ('127.0.0.1',)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
