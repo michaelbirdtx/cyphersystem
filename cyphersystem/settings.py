5e2fae1ab9fa92bd85351ef30ee37d69fe1e14fb
Michael Bird <michael@Michaels-iMac.local>
2018-07-31 18:54:57 -0500

    Created Numenera app.

--- cyphersystem/settings.py
+++ cyphersystem/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'cyphercore.apps.CyphercoreConfig',
+    'numenera.apps.NumeneraConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- cyphersystem/urls.py
+++ cyphersystem/urls.py
@@ -22,6 +22,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('cyphercore/', include('cyphercore.urls')),
+    path('numenera/', include('numenera.urls')),
     path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
