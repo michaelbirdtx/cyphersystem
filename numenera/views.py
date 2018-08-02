f54e9cf63d22211dc5f5af4c09537d0dd3b7f464
Michael Bird <michael@Michaels-iMac.local>
2018-08-01 22:53:08 -0500

    First release of Numenera app.

--- numenera/views.py
+++ numenera/views.py
@@ -11,84 +11,84 @@ BASE_LOGIN_URL = '/admin/login/'
 
 @login_required(login_url=BASE_LOGIN_URL)
 def index(request):
-    return render(request, 'cyphercore/index.html')
+    return render(request, 'numenera/index.html')
 
 class AbilityDetailView(LoginRequiredMixin, generic.DetailView):
     login_url = BASE_LOGIN_URL
     model = Ability
-    template_name = 'cyphercore/ability_detail.html'
+    template_name = 'numenera/ability_detail.html'
 
 class AbilityListView(LoginRequiredMixin, generic.ListView):
     login_url = BASE_LOGIN_URL
     model = Ability
-    template_name = 'cyphercore/ability_list.html'
+    template_name = 'numenera/ability_list.html'
 
 class DescriptorDetailView(LoginRequiredMixin, generic.DetailView):
     login_url = BASE_LOGIN_URL
     model = Descriptor
-    template_name = 'cyphercore/descriptor_detail.html'
+    template_name = 'numenera/descriptor_detail.html'
 
 class DescriptorListView(LoginRequiredMixin, generic.ListView):
     login_url = BASE_LOGIN_URL
     model = Descriptor
-    template_name = 'cyphercore/descriptor_list.html'
+    template_name = 'numenera/descriptor_list.html'
 
 class EquipmentListView(LoginRequiredMixin, generic.ListView):
     login_url = BASE_LOGIN_URL
     model = Equipment
-    template_name = 'cyphercore/equipment_list.html'
+    template_name = 'numenera/equipment_list.html'
 
 class FocusListView(LoginRequiredMixin, generic.ListView):
     login_url = BASE_LOGIN_URL
     model = Focus
-    template_name = 'cyphercore/focus_list.html'
+    template_name = 'numenera/focus_list.html'
 
 class FocusDetailView(LoginRequiredMixin, generic.DetailView):
     login_url = BASE_LOGIN_URL
     model = Focus
-    template_name = 'cyphercore/focus_detail.html'
+    template_name = 'numenera/focus_detail.html'
 
 class SkillListView(LoginRequiredMixin, generic.ListView):
     login_url = BASE_LOGIN_URL
     model = Skill
-    template_name = 'cyphercore/skill_list.html'
+    template_name = 'numenera/skill_list.html'
 
 class TypeListView(LoginRequiredMixin, generic.ListView):
     login_url = BASE_LOGIN_URL
     model = Type
-    template_name = 'cyphercore/type_list.html'
+    template_name = 'numenera/type_list.html'
 
 class TypeDetailView(LoginRequiredMixin, generic.DetailView):
     login_url = BASE_LOGIN_URL
     model = Type
-    template_name = 'cyphercore/type_detail.html'
+    template_name = 'numenera/type_detail.html'
 
 class CharacterListView(LoginRequiredMixin, generic.ListView):
     login_url = BASE_LOGIN_URL
     model = Character
-    template_name = 'cyphercore/character_list.html'
+    template_name = 'numenera/character_list.html'
 
 class CharacterDetailView(LoginRequiredMixin, generic.DetailView):
     login_url = BASE_LOGIN_URL
     model = Character
-    template_name = 'cyphercore/character_detail.html'
+    template_name = 'numenera/character_detail.html'
 
 class CypherListView(LoginRequiredMixin, generic.ListView):
     login_url = BASE_LOGIN_URL
     model = Cypher
-    template_name = 'cyphercore/cypher_list.html'
+    template_name = 'numenera/cypher_list.html'
 
 class CypherDetailView(LoginRequiredMixin, generic.DetailView):
     login_url = BASE_LOGIN_URL
     model = Cypher
-    template_name = 'cyphercore/cypher_detail.html'
+    template_name = 'numenera/cypher_detail.html'
 
 class ArtifactListView(LoginRequiredMixin, generic.ListView):
     login_url = BASE_LOGIN_URL
     model = Artifact
-    template_name = 'cyphercore/artifact_list.html'
+    template_name = 'numenera/artifact_list.html'
 
 class ArtifactDetailView(LoginRequiredMixin, generic.DetailView):
     login_url = BASE_LOGIN_URL
     model = Artifact
-    template_name = 'cyphercore/artifact_detail.html'
+    template_name = 'numenera/artifact_detail.html'
